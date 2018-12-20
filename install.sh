a94d6b8f7f512a13c3a744de8a31bbbad509722a
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2018-12-20 13:23:46 +0000

    Added .gdbinit file which sets disassembly-flavor to intel

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 [ -f $HOME/.vimrc ] && mv $HOME/.vimrc $HOME/.vimrc.bak
 [ -f $HOME/.bashrc ] && mv $HOME/.bashrc $HOME/.bashrc.bak
 [ -f $HOME/.bash_aliases ] && mv $HOME/.bash_aliases $HOME/.bash_aliases.bak
+[ -f $HOME/.gdbinit ] && mv $HOME/.gdbinit $HOME/.gdbinit.bak
 
 # create symlinks for dot files
 for filename in .[A-Za-z]*
